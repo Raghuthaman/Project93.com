cb684f2db8eab24ce7795e00a7fa60c70a77c417
Raghuthaman <87473667+Raghuthaman@users.noreply.github.com>
2021-08-20 11:17:57 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,30 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCd9a9KY-kypof98srwDCCYyeDp1fsquXg",
+    authDomain: "kwitter-project-d8b97.firebaseapp.com",
+    databaseURL: "https://kwitter-project-d8b97-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-d8b97",
+    storageBucket: "kwitter-project-d8b97.appspot.com",
+    messagingSenderId: "409024183743",
+    appId: "1:409024183743:web:6db99d3486df85b091f50a"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+      user_name = localStorage.getItem("Username");
+      document.getElementById("user_name").innerHTML = "Welcome "+user_name+"!";
+
+      function addroom() {
+             room_name = document.getElementById("room_name").value;
+    
+            localStorage.setItem("Roomname",room_name);
+        
+            window.location = "kwitter_page.html";
+      }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
